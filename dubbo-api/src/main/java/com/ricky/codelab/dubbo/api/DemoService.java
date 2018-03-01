789807590fbf1eecf616095d740cea20aa1d49a2
Administrator <Administrator@192.168.1.102>
2018-03-01 23:03:54 +0800

    2222

--- dubbo-api/src/main/java/com/ricky/codelab/dubbo/api/DemoService.java
+++ dubbo-api/src/main/java/com/ricky/codelab/dubbo/api/DemoService.java
@@ -2,6 +2,11 @@ package com.ricky.codelab.dubbo.api;
 
 import com.ricky.codelab.dubbo.api.model.User;
 
+/**
+ * ≤‚ ‘
+ * @author Administrator
+ *
+ */
 public interface DemoService {
  
 	public String sayHello(String name);
